cbd09488ffcb977911279d84c446a10e8b1eb156
kkensuke <n4659017382@gmail.com>
2024-07-29 11:12:31 +0900

    ✨ NEW: tag feature in markdown

---
+++ app/blog/tags/[slug]/page.tsx
@@ -0,0 +1,47 @@
+import fs from "fs";
+import Link from "next/link";
+import matter from "gray-matter";
+import { PostMetadata } from '../../../../components/PostMetadata';
+import PostPreview from "../../../../components/PostPreview";
+import getPostMetadata from "../../../../components/getPostMetadata";
+
+
+const getTagPosts = (tag: string): PostMetadata[] => {
+  const posts = getPostMetadata();
+  return posts.filter((post) => post.tags?.includes(tag));
+};
+
+type TagPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+const TagPage = ({ params }: TagPageProps) => {
+  const { slug } = params;
+  const posts = getTagPosts(slug);
+
+  return (
+    <div className="mx-auto max-w-screen-md">
+      <div className="my-8 text-center text-4xl text-slate-600">
+        <h1>Posts tagged with "{slug}"</h1>
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        {posts.map((post) => (
+          <PostPreview key={post.slug} {...post} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TagPage;
+
+export async function generateStaticParams() {
+  const posts = getPostMetadata();
+  const tags = Array.from(new Set(posts.flatMap(post => post.tags || [])));
+
+  return tags.map((tag) => ({
+    slug: tag,
+  }));
+}
